64f065f8817db91baec952fb8fe963598d56fa83
alex.munz <a_munz@list.ru>
2019-04-06 20:50:19 +0300

    Доделывет второй экран

--- src/assets/js/components/works.js
+++ src/assets/js/components/works.js
@@ -4,8 +4,12 @@ const slider = new Swiper('#works-slider', {
   slidesPerView: 3,
   centeredSlides: true,
   loop: true,
+  navigation: {
+    nextEl: '.works__button_next',
+    prevEl: '.works__button_prev',
+  },
+  allowTouchMove: false,
   slideActiveClass: 'works__item_active',
   slideNextClass: 'works__item_next',
   slidePrevClass: 'works__item_prev'
-
 });
